80c46ada533b2988a8445d8996dccb191d5fa622
Dmitriy <dmitriy.babich0@gmail.com>
2021-06-19 21:02:30 -0700

    can add and view all 3 tables

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ const renderEmployee = () => {
     const query = `SELECT * FROM employees`;
     connection.query(query,(err,res) =>{
         res.forEach(({id,first_name,last_name,role_id,manager_id})=>{
-            console.log(`id: ${id}  ||  first: ${first_name}   ||  last: ${last_name}    ||  role: ${role_id}  ||  manager: ${manager_id}`);
+            console.table(res);
             employeeSearch();
         })
     })
@@ -131,7 +131,7 @@ const renderDepartment = () => {
     const query = `SELECT * FROM department`;
     connection.query(query,(err,res)=>{
         res.forEach(({id,name})=>{
-            console.log(`id: ${id}  ||  name: ${name}`);
+            console.table(res);
             employeeSearch();
         })
     })
@@ -141,7 +141,7 @@ const renderRole = () => {
     const query = `SELECT * FROM role`;
     connection.query(query,(err,res) =>{
         res.forEach(({id,title,salary,department_id})=>{
-            console.log(`id: ${id}  ||  title: ${title}   ||  salary: ${salary}    ||  department_id: ${department_id}`);
+            console.table(res);
             employeeSearch();
         })
     })
@@ -149,16 +149,121 @@ const renderRole = () => {
 
 // add to table functions
 const addToEmployee = () => {
-    // code
+    let query = `SELECT role.id, role.title FROM role ORDER BY role.id`;
+    connection.query(query, (err,res)=>{
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: 'firstName',
+                type:'input',
+                message:'Employee First name:',
+            },
+            {
+                name: 'lastName',
+                type:'input',
+                message:'Employee Last name:',
+            },
+            {
+                name: 'roleId',
+                type:'list',
+                choices: res.map(()=>res[0].id),
+                message:'Employee role id',
+                
+            },
+            {
+                name: 'managerId',
+                type:'input',
+                message:"Employee's Manager id (leave blank if none)",
+
+            },
+        ]).then((answers)=>{
+            if (answers.managerId === '') {
+                answers.managerId = 0;
+            };
+            connection.query(
+                `INSERT INTO employees SET ?`,
+                
+                {
+                    first_name: answers.firstName,
+                    last_name: answers.lastName,
+                    role_id: answers.roleId,
+                    manager_id: answers.managerId,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log(`employee ${answers.firstName} was added to the employee database`);
+                    employeeSearch();
+                }
+            )
+
+        });
+    });
+
 };
 
 const addToDepartment = () => {
-    // code
+    inquirer.prompt([
+        {
+            name: 'name',
+            type:'input',
+            message:'Department name: ',
+        },
+    ]).then((answers)=>{
+        connection.query(
+            `INSERT INTO department SET ?`,
+
+            {
+                name: answers.name,
+            },
+            (err) => {
+                if (err) throw err;
+                console.log(`Department ${answers.name} was added to the department database`);
+                employeeSearch();
+            }
+        )
+    });
 };
 
+
 const addToRole = () => {
-    // code
+    let query = 'SELECT role.id, role.title FROM role ORDER BY role.id';
+    connection.query(query, (err,res)=>{
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name:'title',
+                type: 'input',
+                message:'New role title'
+            },
+            {
+                name:'salary',
+                type:'integer',
+                message:'sallary  ex.(00.000)'
+            },
+        ])
+        .then((answers)=>{
+            let id = res.map(()=>res[0].id)
+            id++;
+            console.log('Role Id: ' + id);
+            connection.query(
+                'INSERT INTO role SET ?',
+                
+                {
+                    title: answers.title,
+                    salary: answers.salary,
+                    department_id: id,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log(`Department ${answers.name} was added to the department database`);
+                    employeeSearch();
+                },
+                
+            )
+        })
+    });
 };
+
 
 const updateEmployee = () => {
     // code
@@ -175,5 +280,5 @@ const updateRole = () => {
 const endConnection = () => {
     connection.end();
     console.log('Exiting......');
-}
+};
 
---
+++ seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO department(name)
+VALUES('Sales');
+
+INSERT INTO role (title,salary,department_id)
+VALUES('Sales person','75.000',1);
+
+INSERT INTO employees (first_name,last_name,role_id)
+VALUES('Alex','Snyder',1);
